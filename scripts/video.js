170a61a07d95fff24e743d730bb2921aed02c548
Rakib Patwary <142200450+rakibpatwary44@users.noreply.github.com>
2025-07-15 12:02:33 +0600

    first commit

---
+++ scripts/video.js
@@ -0,0 +1,86 @@
+console.log("video scripts added")
+
+
+const loadCategories = () => {
+    fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
+        .then(res => res.json())
+        .then(data => displayCategories(data.categories))
+        .catch(error => console.log(error))
+}
+
+
+const loadVideos = () => {
+    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+        .then(res => res.json())
+        .then(data => displayVideos(data.videos))
+        .catch(error => console.log(error))
+
+}
+
+
+const cardDamo = {
+    "category_id": "1001",
+    "video_id": "aaaa",
+    "thumbnail": "https://i.ibb.co/L1b6xSq/shape.jpg",
+    "title": "Shape of You",
+    "authors": [
+        {
+            "profile_picture": "https://i.ibb.co/D9wWRM6/olivia.jpg",
+            "profile_name": "Olivia Mitchell",
+            "verified": ""
+        }
+    ],
+    "others": {
+        "views": "100K",
+        "posted_date": "16278"
+    },
+    "description": "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
+}
+
+const displayVideos = (videos) => {
+    const videosContainer = document.getElementById("videos")
+    console.log(videos)
+    videos.forEach(video => {
+        console.log(video);
+
+        const card = document.createElement("div");
+        card.classList = "card card-compact"
+        card.innerHTML = `
+        <figure>
+    <img
+      src=${video.thumbnail}
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">Card Title</h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+         `;
+        videosContainer.append(card);
+
+    });
+}
+
+const displayCategories = (categories) => {
+
+    const categoriesContainer = document.getElementById('category')
+
+    categories.forEach(item => {
+
+        console.log(item)
+
+
+        const button = document.createElement("button");
+        button.classList = "btn"
+        button.innerText = item.category
+
+        categoriesContainer.append(button);
+    });
+
+}
+
+loadCategories()
+loadVideos()
